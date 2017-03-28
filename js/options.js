dc9a93e3f802b428ced327d37952e6d096bc0b34
alicj <zichen.jiang@yahoo.com>
2017-03-27 22:20:43 -0400

    complete #10

--- js/options.js
+++ js/options.js
@@ -6,39 +6,35 @@ function isValidUrl(url) {
 	return url.match(regex);
 }
 
-$(document).on('click', '#urlForm .submit', function(event) {
+$(document).on('submit', '#urlForm', function(event) {
 	event.preventDefault();
-	var name = $('#urlForm #name').val();
-	var url = $('#urlForm #url').val();
+	var data = {};
+	$(this).serializeArray().map(function(x){data[x.name] = x.value;}); 
 
-	if (!isValidUrl(url)){
+	if (!isValidUrl(data.url)){
 		$('#urlForm .form-group').addClass('has-error');
 		$('#urlForm .help-block').text('Invalid URL');
 		return;
 	}
 
-	var newUrl = {
-		name: name,
-		url: url
-	};
-
 	STORAGE.get('urls', function(item){
 		if (item.urls === undefined) {
-			item.urls = [newUrl];
+			item.urls = [data];
 		} else {
 			var duplicate = false;
 			$.each(item.urls, function(index, val) {
-				if (val.name == newUrl.name) {
+				if (val.name == data.name) {
 					duplicate = true;
-					item.urls[index] = newUrl;
+					item.urls[index] = data;
 				}
 			});
 			if (!duplicate) {
-				item.urls.push(newUrl);
+				item.urls.push(data);
 			}
 		}
 		STORAGE.set(item, function(){
 			updateUrl();
+			$('#urlForm')[0].reset();
 		});
 	});
 
@@ -51,10 +47,17 @@ $(document).on('click', '#deleteAllUrl', function(event) {
 
 function updateUrl(){
 	STORAGE.get('urls', function(item){
-					console.log('update urls', item)
+		console.log('update urls', item)
 		var listing = '';
 		$.each(item.urls, function(index, val) {
-			listing += '<tr><td>'+ val.name +'</td><td>' + val.url + '</td></tr>';
+			listing += `
+			<tr>
+				<td>${val.name}</td>
+				<td>${val.url}</td>
+				<td>${val.method}</td>
+				<td>${val.comment}</td>
+			</tr>
+			`;
 		});
 		$('#urlList').html(listing);
 	});
